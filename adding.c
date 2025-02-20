6d3e63adfa6e24ebcbf6ed720859b1cb06a5518d
student <student@aublru.amity.edu.in>
2025-02-20 11:33:27 +0530

    adding.c is a c file of adding two numbers, adding.py is a python file of adding two numbers

---
+++ adding.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Enter the 1st number: ");
+	scanf("%d",&a);
+	printf("Enter the 2st number: ");
+        scanf("%d",&b);
+	sum=a+b;
+	printf("the sum of %d and %d is:%d"a,b,sum);
+	return 0;
